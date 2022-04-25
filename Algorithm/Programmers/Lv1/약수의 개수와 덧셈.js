846534fe2c7f9dda947cc098eac633a7d3e1040a
applehole <skyheo123@naver.com>
2022-04-25 11:39:29 +0900

    Algorithm: 약수의 개수와 덧셈

---
+++ Algorithm/Programmers/Lv1/약수의 개수와 덧셈.js
@@ -0,0 +1,24 @@
+function solution(left, right) {
+    var answer = 0;
+    for(let i=left; i<=right; i++){
+        let count = 0
+        for(let j=0; j<=i; j++){
+            if(i%j===0){
+                if(j*j===i){
+                    count++
+                }else{
+                 count= count+2
+                }
+            }
+        }
+        if(count%2===0){
+            answer = answer + i
+        }
+        else {
+            answer = answer -i
+        }
+    }
+    return answer;
+}
+
+//link https://programmers.co.kr/learn/courses/30/lessons/77884
