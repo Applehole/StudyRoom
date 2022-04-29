fa39848cf4d77911bb8fd8b4f3c859c8f9e2a66a
applehole <skyheo123@naver.com>
2022-04-30 00:05:19 +0900

    Algorithm: 나누어 떨어지는 숫자배열

---
+++ Algorithm/Programmers/나누어 떨어지는 숫자배열.js
@@ -0,0 +1,14 @@
+function solution(arr, divisor) {
+  var answer = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % divisor === 0) {
+      answer.push(arr[i]);
+    }
+  }
+  if (answer.length === 0) {
+    answer.push(-1);
+  }
+  return answer.sort((a, b) => a - b);
+}
+
+//link https://programmers.co.kr/learn/courses/30/lessons/12910
