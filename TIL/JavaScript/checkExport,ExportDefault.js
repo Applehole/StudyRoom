ad9f94de56c378040f9d6901d0bb6e07a8fff11d
applehole <skyheo123@naver.com>
2022-05-02 21:11:13 +0900

    TIL: export temp

---
+++ TIL/JavaScript/checkExport,ExportDefault.js
@@ -0,0 +1,9 @@
+// import first from "./export";
+import add from "./exportDefault.js";
+
+let a = 10,
+  b = 5;
+
+// console.log(first(a, b));
+console.log(add(a, b));
+// console.log(minus(a, b));
---
+++ TIL/JavaScript/export.js
@@ -0,0 +1,3 @@
+export function double(a, b) {
+  return a * b;
+}
