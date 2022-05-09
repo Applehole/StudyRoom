be6f0c4476052d08c43bcf16bbe7860eaed92325
applehole <skyheo123@naver.com>
2022-05-09 19:20:20 +0900

    Fix: 파일 옮김

---
+++ TIL/JavaScript/객체.js
@@ -0,0 +1,15 @@
+let A = {
+  first: 1,
+  second: 2,
+  third: 3,
+  fourth: 4,
+  fifth: 5,
+  sixth: 6,
+  seventh: 7,
+  eighth: 8,
+  ninth: 9,
+};
+
+A.forEach((el) => {
+  console.log(el);
+});
