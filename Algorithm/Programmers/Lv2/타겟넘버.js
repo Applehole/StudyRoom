bdf97cee1b035f16755077d3e90fb35787ebb092
Applehole <skyheo123@naver.com>
2022-05-25 11:17:23 +0900

    Algorithm: 타겟넘버

--- Algorithm/Programmers/Lv2/타겟넘버.js
+++ Algorithm/Programmers/Lv2/타겟넘버.js
@@ -1,28 +1,28 @@
-function solution(numbers, target) { // 타겟을 빼서 모든 경우의 수를 만든 다음, 경우의 수만큼 더해준다.
-    var answer = 0;
-    const AddEveryThing = numbers.reduce((acc,cur)=>{
-       return acc = acc+cur 
-    },0)
-    console.log(AddEveryThing)
-    const GapOfNumber = AddEveryThing-target
-    
-  let MathClass=[]
-  let Storage=[[]]
-  
-   for(let i=0; i<numbers.length; i++){
-     for(let j=0; j<Storage.length; j++){
-        MathClass.push([...Storage[j],numbers[i]])
-     }
-     Storage = Storage.concat(MathClass)
-     MathClass=[]
-   }
-    Storage.map((el)=>{
-       let number = el.reduce((acc,cur)=>{
-            return acc = acc+cur
-        },0)
-       if(number === GapOfNumber){
-           answer++
+function solution(numbers, target) {
+   let Storage = []
+   let finalAnswer = 0
+   
+   function something(count, result, Array){
+       if(Array.length===0){
+           Storage.push(result)
+           return
        }
-    })
-    return answer;
+       let head = Array [0]
+       let tail = Array.slice(1)
+       
+       something(count-1, result.concat(head), tail )
+       something(count-1, result.concat(-head), tail )
+   }
+   something(numbers.length, [], numbers)
+   
+   Storage.map((el)=>{
+       let sum =el.reduce((acc,cur)=>{
+          return acc= acc+cur 
+       },0)
+       if(sum===target){
+           finalAnswer++
+       }
+   })
+   
+   return finalAnswer
 }
