2fa0d12ec88e598f5fea3f9b8e4ab5d72aa281ae
Applehole <skyheo123@naver.com>
2022-06-07 17:27:02 +0900

    TIL: typeScript

---
+++ TIL/TypeScripte/errorOfJs.js
@@ -0,0 +1,13 @@
+console.log("array",[1,2,3]+false)
+
+function add (a,b){
+    return a+b
+}
+
+function divide(a,b){
+    return b/a
+}
+
+console.log("add",add("xxxxx"))
+
+console.log("divide",divide("2222"))
