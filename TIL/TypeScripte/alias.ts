24d69f603bff14ca8a76d97ab0cdfa8fecec7b51
Applehole <skyheo123@naver.com>
2022-06-08 13:55:42 +0900

    TIL: alias

--- TIL/TypeScripte/alias.ts
+++ TIL/TypeScripte/alias.ts
@@ -1,16 +1,16 @@
 
 //const나 let 타입의 정의
-type PlayerAge = number
+type PlayerAge = number // 타입을 정의하고 싶으면 type을 앞에 써주면 된다.
 type PlayerName = string 
 
 type Player ={
-    readonly name:PlayerName,
+    readonly name:PlayerName, // 호출만 가능하고, 변경이 불가능 해진다.
     age?:PlayerAge
 }
 
 const nico : Player ={
     name:"nico"
-}
+} // 
 
 const injae : Player ={
     name:"injae",
