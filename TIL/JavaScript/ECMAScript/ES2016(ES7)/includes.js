6b1d2c7d799ab3a5e4ea6738f6070f521ec5c042
Applehole <skyheo123@naver.com>
2022-06-09 14:51:14 +0900

    TIL: ES2016

--- TIL/JavaScript/ECMAScript/ES2016(ES7)/includes.js
+++ TIL/JavaScript/ECMAScript/ES2016(ES7)/includes.js
@@ -3,7 +3,8 @@ let 떡잎방범대 = ["짱구", "철수", "맹구", "유리", "훈이"]
 떡잎방범대.includes("신봉선") // false
 떡잎방범대.includes("짱구") //true
 
-떡잎방범대.includes("짱구",1)
+떡잎방범대.includes("짱구",1) // false
+떡잎방범대.includes("맹구",1) // true
 
 console.log(떡잎방범대.includes("신봉선"));
 console.log(떡잎방범대.includes("짱구"));
