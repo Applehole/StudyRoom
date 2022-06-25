6e07dc7e09569df7b8f86527f9f1b0c25a73fcb0
Applehole <skyheo123@naver.com>
2022-06-25 22:01:15 +0900

    TIL: HOC

--- TIL/React/hoc/src/HOC.js
+++ TIL/React/hoc/src/HOC.js
@@ -1,36 +1,36 @@
 import React from "react";
 
-const Hoc = (WrappedComponent, entity) =>{
-    return class extends React.Component{
+const Hoc = (WrappedComponent, entity) => {
+    return class extends React.Component {
         state = {
             data: [],
             term: ""
         };
-        componentDidMount(){
+        componentDidMount() {
             const fetchData = async () => {
                 const res = await fetch(`https://jsonplaceholder.typicode.com/${entity}`);
                 const json = await res.json();
-                this.setState({...this.state, data: json});
+                this.setState({ ...this.state, data: json });
             };
             fetchData();
         }
-        render(){
-            let {term, data} = this.state;
-            let filteredData = data.slice(0,10).filter((d) => {
-                if(entity==="users"){
-                    const {name} = d;
+        render() {
+            let { term, data } = this.state;
+            let filteredData = data.slice(0, 10).filter((d) => {
+                if (entity === "users") {
+                    const { name } = d;
                     return name.indexOf(term) >= 0
                 }
-                if(entity==="todos"){
-                    const {title} = d;
+                if (entity === "todos") {
+                    const { title } = d;
                     return title.indexOf(term) >= 0
                 }
             });
-            return(
+            return (
                 <div>
                     <h2>{entity}</h2>
-                    <input type="text" value={term} onChange={(e) => this.setState({...this.state, term: e.target.value})} />
-                    <WrappedComponent data= {filteredData}></WrappedComponent>
+                    <input type="text" value={term} onChange={(e) => this.setState({ ...this.state, term: e.target.value })} />
+                    <WrappedComponent data={filteredData}></WrappedComponent>
                 </div>
             )
         }
