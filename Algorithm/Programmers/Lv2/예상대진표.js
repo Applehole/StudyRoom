7445fabf00fbc27c2b5af0fff19a7e35d75f36b9
Applehole <skyheo123@naver.com>
2022-07-02 20:48:28 +0900

    TIL: 예상대진표

--- Algorithm/Programmers/Lv2/예상대진표.js
+++ Algorithm/Programmers/Lv2/예상대진표.js
@@ -1,11 +1,21 @@
 function solution(n,a,b){
     let result = 1
     for(let i=0; i<n; i++){
-        if(Math.abs(a-b)===1){
-            return result
+        if(Math.abs(a-b)){
+            if(!a%2){
+                if(a-1===b){
+                    return result
+                }
+            }if(a%2){
+                if(a+1===b){
+                    return result
+                }
+            }
         }
         a = Math.ceil(a/2)
         b = Math.ceil(b/2)
         result++
     }
 }
+
+solution(8,4,5)
