7808f8e56d398b67412adce5cd0c5ee2f13e17fc
Applehole <skyheo123@naver.com>
2022-07-09 23:19:24 +0900

    TIL: typeScript blockchain last

--- TIL/TypeScripte/TypeChain/src/index.ts
+++ TIL/TypeScripte/TypeChain/src/index.ts
@@ -22,6 +22,24 @@ class Block implements BlockShape{
     }
     static calculateData(PrevHash:string, height:number, data:string){
         const toHash = `${PrevHash}${height}${data}`
-        return toHash
+        return crypto.createHash("sha256").update(toHash).digest("hex")
+    }
+}
+
+class BlockChain {
+    private blocks: Block[]
+    constructor(){
+        this.blocks = [];
+    }
+    private getPrevHash(){
+        if(this.blocks.length === 0) return ""
+        return this.blocks[this.blocks.length -1 ].hash
+    }
+    public addBlock(data:string){
+        const newBlock = new Block(this.getPrevHash(), this.blocks.length+1, data);
+        this.blocks.push(newBlock);
+    }
+    public getBlocks(){
+        return [...this.blocks];
     }
 }
