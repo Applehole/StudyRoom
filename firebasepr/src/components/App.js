a548284f900dd4d106a163ee13ef254677aa2884
Applehole <skyheo123@naver.com>
2022-07-15 18:37:53 +0900

    Fix: fix the firebase connection

--- firebasepr/src/components/App.js
+++ firebasepr/src/components/App.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
 import AppRouter from "./router";
+import { authService } from "../firebase";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  console.log(authService.currentUser)
   return (
-    <AppRouter />
+    <>
+    <AppRouter isLoggedIn={isLoggedIn} />
+    <footer> &copy; Practice {new Date().getFullYear()}</footer>
+    </>
   );
 }
 
--- firebasepr/src/firebase.js
+++ firebasepr/src/firebase.js
@@ -1,19 +1,19 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
+
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTHDOMAIN,
-  projectId: process.env.REACT_APP_PROJECTED,
-  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGEINGSENDERID,
-  appId: process.env.REACT_APP_APPID
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_FIREBASE_APPID
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const fbase = initializeApp(firebaseConfig);
 
-export default app;
+
+export const authService = getAuth(fbase);
+
