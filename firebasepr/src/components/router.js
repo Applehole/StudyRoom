2a5a1fd4682d7f97e040597693cf436451cf9f0a
Applehole <skyheo123@naver.com>
2022-07-15 10:31:08 +0900

    Feat: Router apply

--- firebasepr/src/components/router.js
+++ firebasepr/src/components/router.js
@@ -1,27 +1,25 @@
 import React, { useState } from "react";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
 
 const AppRouter = () => {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
-    <Router>
-        <Switch>
+    return(
+    <BrowserRouter>
+        <Routes>
             {isLoggedIn ? (
                 <>
-                    <Route exact path="/">
-                        <Home />
-                    </Route>
+                    <Route path="/" element={<Home />} />
                 </>
             )
                 : (
-                    <Route exact path="/">
-                        <Auth />
-                    </Route>
+                    <Route path="/" element={<Auth />} />
                 )
             }
-        </Switch>
-    </Router>
+        </Routes>
+    </BrowserRouter>
+    )
 }
 
 export default AppRouter;
