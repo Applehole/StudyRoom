b397dc356e9f0b8d44beb0a343da56ce42a1d6a8
Applehole <skyheo123@naver.com>
2022-07-29 21:54:39 +0900

    TIL: Hooks

---
+++ TIL/React/Hooks/Hooks.js
@@ -0,0 +1,20 @@
+
+// // class
+// componentDidMount() {
+// 	this.updateList(this.props.id);
+// }
+
+// componentDidUpdate(prevProps) {
+// 	if (prevProps.id !== this.props.id) {
+//     	this.updateList(this.props.id);
+//     }
+// }
+
+// updateList = () => {
+// 	feachList(id).then((list) => this.setState({list}));
+// }
+
+// // hooks
+// useEffect(() => {
+// 	feachList(id).then((res) => setRes(res));
+// }, [id]);
