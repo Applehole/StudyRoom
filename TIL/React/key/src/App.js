eb1c8abc72da803cf25b440a2f0fd4274703fad0
Applehole <skyheo123@naver.com>
2022-08-09 20:05:24 +0900

    TIL: Key

--- TIL/React/key/src/App.js
+++ TIL/React/key/src/App.js
@@ -1,51 +1,75 @@
 import React, { useState } from "react";
 import "./app.css"
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
-  const [inputValue, setInputValue] = useState("")
-  const [list, setList] = useState(["코딩", "밥", "잠"])
-  const addToList = () => {
-    setList((currentValue) => {
-      return [...currentValue, inputValue]
+  const [withoutErrorValue, setWithoutErrorValue] = useState("")
+  const [indexErrorValue, setIndexErrorValue] = useState("")
+  const [overlapErrorValue, setOverlapErrorValue] = useState("")
+  const [withoutlist, setWithoutList] = useState(["키값이 없어","여기는 에러가 생기겠지"])
+  const [indexlist, setIndexList] = useState(["인덱스 에러가 생겨","맞아"])
+  const [overlapList, setOverlapList] = useState(["중복값이 들어가면 생겨","그래?"])
+
+  const withOutAddToList = () => {
+    setWithoutList((currentValue) => {
+      return [withoutErrorValue , ...currentValue]
     });
-    setInputValue("");
+    setWithoutErrorValue("");
+  }
+  const IndexAddToList = () => {
+    setIndexList((currentValue) => {
+      return [indexErrorValue , ...currentValue]
+    });
+    setIndexErrorValue("");
+  }
+  const overLapAddToList = () => {
+    setOverlapList((currentValue) => {
+      return [overlapErrorValue, ...currentValue ]
+    });
+    setOverlapErrorValue("");
   }
 
   return (
     <>
       <div className="appDiv">
-        <div className="withoutError">
-          <h1>키 값 에러 없이</h1>
-          <input value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
-          <button onClick={addToList}>추가</button>
+      <div className="withoutError">
+          <h1>키값이 존재하지 않음</h1>
+          <input value={withoutErrorValue} onChange={(e) => setWithoutErrorValue(e.target.value)} />
+          <button onClick={withOutAddToList}>추가</button>
           <ul>
-            {list.map((string) => {
+            {withoutlist.map((string) => {
               return (
-                <li>{string}</li>
+                  <li>{string}</li>
               )
             })}
           </ul>
         </div>
         <div className="indexError">
           <h1>키 값 index</h1>
-          <input value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
-          <button onClick={addToList}>추가</button>
+          <input value={indexErrorValue} onChange={(e) => setIndexErrorValue(e.target.value)} />
+          <button onClick={IndexAddToList}>추가</button>
           <ul>
-            {list.map((string) => {
+            {indexlist.map((string, index) => {
               return (
-                <li>{string}</li>
+                <div key={index}>
+                  <li>{string}</li>
+                  <input/>
+                </div>
               )
             })}
           </ul>
         </div>
         <div className="overlapError">
           <h1>키 값 중복</h1>
-          <input value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
-          <button onClick={addToList}>추가</button>
+          <input value={overlapErrorValue} onChange={(e) => setOverlapErrorValue(e.target.value)} />
+          <button onClick={overLapAddToList}>추가</button>
           <ul>
-            {list.map((string) => {
+            {overlapList.map((string) => {
               return (
-                <li>{string}</li>
+                <div key={string}>
+                  <li>{string}</li>
+                  <input/>
+                </div>
               )
             })}
           </ul>
