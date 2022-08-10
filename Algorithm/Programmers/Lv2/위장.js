c949b1d3a8a143119f0abe1ccd5dd98340ecc733
Applehole <skyheo123@naver.com>
2022-08-10 23:08:41 +0900

    Algorithm: 위장

--- Algorithm/Programmers/Lv2/위장.js
+++ Algorithm/Programmers/Lv2/위장.js
@@ -1,23 +1,36 @@
+// function solution(clothes) {
+//     let tempArray = []
+//     let tempNumberArray = []
+
+//     for (let i = 0; i < clothes.length; i++) {
+//         if (tempArray.includes(clothes[i][1])) {
+//             tempNumberArray[tempArray.findIndex((el) => el === clothes[i][1])] = tempNumberArray[tempArray.findIndex((el) => el === clothes[i][1])] + 1
+//         } else {
+//             tempArray.push(clothes[i][1])
+//             tempNumberArray.push(1)
+//         }
+//     }
+//     if (tempArray.length === 1) {
+//         return clothes.length
+//     }
+//     let creduceEveryNumber = tempNumberArray.reduce((cur, acc) => {
+//         return cur = cur * acc
+//     }, 1)
+
+//     return clothes.length + creduceEveryNumber
+// }
+
 function solution(clothes) {
-    let tempArray = []
-    let tempNumberArray = []
-
-    for (let i = 0; i < clothes.length; i++) {
-        if (tempArray.includes(clothes[i][1])) {
-            tempNumberArray[tempArray.findIndex((el) => el === clothes[i][1])] = tempNumberArray[tempArray.findIndex((el) => el === clothes[i][1])] + 1
-        } else {
-            tempArray.push(clothes[i][1])
-            tempNumberArray.push(1)
-        }
+    var answer = 0;
+    const spyWear = {}
+    for(const clothNPart of clothes){
+        if(spyWear[clothNPart[1]] === undefined) spyWear[clothNPart[1]] = []
+        spyWear[clothNPart[1]].push(clothNPart[0])
     }
-    if (tempArray.length === 1) {
-        return clothes.length
-    }
-    let creduceEveryNumber = tempNumberArray.reduce((cur, acc) => {
-        return cur = cur * acc
-    }, 1)
-
-    return clothes.length + creduceEveryNumber
+    const clothesCount = []
+    for(const part in spyWear) clothesCount.push(spyWear[part].length + 1)
+    answer = clothesCount.reduce((previous, current) => previous * current, 1) - 1
+    return answer;
 }
 
 solution([["yellow_hat", "headgear"], ["blue_sunglasses", "eyewear"], ["green_turban", "headgear"], ["crow_mask", "face"], ["blue_sunglasses", "face"], ["smoky_makeup", "face"]])
