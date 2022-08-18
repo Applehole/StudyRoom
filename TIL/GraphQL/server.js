220bb92be2926e0af1c702b6349c13cb647353c1
Applehole <skyheo123@naver.com>
2022-08-18 22:39:39 +0900

    TIL: Proto Graphql

--- TIL/GraphQL/server.js
+++ TIL/GraphQL/server.js
@@ -1,6 +1,24 @@
 import { ApolloServer, gql } from "apollo-server";
 
-const server = new ApolloServer({})
+const typeDefs = gql`
+type User {
+    id: ID
+    age: Int
+    name: String
+}
+
+type tweet {
+    id:ID
+    text: String
+    creator: User
+}
+type Query {
+    allTweet:[tweet]
+    Tweet: tweet
+}
+`
+
+const server = new ApolloServer({typeDefs})
 
 server.listen().then(({url})=>{
     console.log(`Running on ${url}`);
