5c24325fb5bcc7d4fbf88a91e3607999fad3248e
Applehole <skyheo123@naver.com>
2022-08-19 16:00:29 +0900

    TIL: GraphQL Null

--- TIL/GraphQL/server.js
+++ TIL/GraphQL/server.js
@@ -1,21 +1,28 @@
 import { ApolloServer, gql } from "apollo-server";
+//!를 가지고 있으면 null값이 나오지 않는다는 뜻 == 모든 정보에 들어가 있는 값인지 판단하기
 
 const typeDefs = gql`
 type User {
-    id: ID
-    age: Int
-    name: String
+    id: ID!
+    age: Int!
+    name: String!
 }
 
 type tweet {
-    id:ID
-    text: String
-    creator: User
+    id:ID!
+    text: String!
+    creator: User!
 }
 type Query {
-    allTweet:[tweet]
-    Tweet(id: ID): tweet
+    allTweet:[tweet!]!
+    Tweet(id: ID!): tweet
 }
+
+type Mutation {
+    postTweet(text: String!, userId: ID!): tweet!
+    deleteTweet(id:ID!): Boolean!
+}
+
 `
 
 const server = new ApolloServer({typeDefs})
