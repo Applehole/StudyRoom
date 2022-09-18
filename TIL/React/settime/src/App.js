b47c5de76e43ff98251d98355793bd02d78cd060
Applehole <skyheo123@naver.com>
2022-09-18 15:49:16 +0900

    TIL: useEffect learning

---
+++ TIL/React/settime/src/App.js
@@ -0,0 +1,11 @@
+import UseEffectNormal from "./useEffect";
+
+function App() {
+  return (
+    <div className="App">
+      <UseEffectNormal />
+    </div>
+  );
+}
+
+export default App;
---
+++ TIL/React/settime/src/useEffect.js
@@ -0,0 +1,22 @@
+import React, { useEffect } from "react";
+
+function UseEffectNormal() {
+  useEffect(() => {
+    console.log("1 첫번째");
+  });
+
+  useEffect(() => {
+    console.log("2 두번째");
+  });
+
+  useEffect(() => {
+    console.log("3 두번째");
+  });
+
+  //해당 컴포넌트로 알 수 있는 사실
+  // useEffect는 위에서 아래로 실행된다.
+
+  return <div className="App"></div>;
+}
+
+export default UseEffectNormal;
